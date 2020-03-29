52c0ddb6d11567841fe0b184e2b73891e9635428
Adrian Pilarczyk <adrian@useo.pl>
2020-03-29 17:18:43 +0200

    Add user generator

---
+++ generators/user.js
@@ -0,0 +1,16 @@
+const faker = require('faker');
+
+const getRandomGender = () => Math.floor(Math.random() * 2)
+
+const getRandomBirthDate = () => faker.date.between('1980-01-01', '2002-12-31');
+
+const getRandomUser = () => ({
+  first_name: faker.name.firstName(),
+  last_name: faker.name.lastName(),
+  birth_date: getRandomBirthDate(),
+  gender: getRandomGender(),
+  photo_url: faker.image.avatar(),
+  description: faker.lorem.sentences(3, 3)
+})
+
+exports.getRandomUser = getRandomUser;
